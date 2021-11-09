99d984eb37673d8bbbc49efc3965e9d691ae208d
nathaniel <nagoldfarb@wpi.edu>
2021-11-08 21:33:23 -0500

    updated plotting methods

--- pythonProject/Averages2D.py
+++ pythonProject/Averages2D.py
@@ -2,68 +2,126 @@ from GaitAnaylsisToolkit.Session import ViconGaitingTrial
 from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
-from GaitCore.Core import Point
+from GaitCore.Core import Point, PointArray
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    fig, ax = plt.subplots(3)
+
     #Change to location of file in your system
-    curFile = r"C:\Users\Owner\Documents\GaitAnalysisCSVs\CSVs\11_12_20_nathaniel_walking_00.csv"
+    curFile = "/home/nathanielgoldfarb/compareMarkerVsExo/EXODataVisualization/pythonProject/Files/11_12_20_nathaniel_walking_00.csv"
     trial = ViconGaitingTrial.ViconGaitingTrial(vicon_file=curFile)
     markers = trial.vicon.get_markers()
     markers.smart_sort()
     # array of marks
-    markingSide = [markers.get_marker("LFemurSide0"), markers.get_marker("LFemurSide1"),
-                   markers.get_marker("LFemurSide2"), markers.get_marker("LFemurSide3")]
-    marking = [markers.get_marker("LFemurFront0"), markers.get_marker("LFemurFront1"),
-               markers.get_marker("LFemurFront2"), markers.get_marker("LFemurFront3"),
-               markers.get_marker("LFemurBack0"), markers.get_marker("LFemurBack1"),
-               markers.get_marker("LFemurBack2"), markers.get_marker("LFemurBack3")]
-    trial_range = len(marking[0])
+    femur_side = [markers.get_marker("LFemurSide0"), markers.get_marker("LFemurSide1"),
+                  markers.get_marker("LFemurSide2"), markers.get_marker("LFemurSide3")]
+
+
+    femur_center = [markers.get_marker("LFemurFront0"), markers.get_marker("LFemurFront1"),
+                    markers.get_marker("LFemurFront2"), markers.get_marker("LFemurFront3"),
+                    markers.get_marker("LFemurBack0"), markers.get_marker("LFemurBack1"),
+                    markers.get_marker("LFemurBack2"), markers.get_marker("LFemurBack3")]
+
+
+
+
+    tibia_side = [markers.get_marker("LTibiaSide0"), markers.get_marker("LTibiaSide1"),
+                  markers.get_marker("LTibiaSide2"), markers.get_marker("LTibiaSide3")]
+
+
+    tibia_center = [markers.get_marker("LTibiaFront0"), markers.get_marker("LTibiaFront1"),
+                markers.get_marker("LTibiaFront2"), markers.get_marker("LTibiaFront3"),
+                markers.get_marker("LTibiaBack0"), markers.get_marker("LTibiaBack1"),
+                markers.get_marker("LTibiaBack2"), markers.get_marker("LTibiaBack3")]
+
+
+    trial_range = len(femur_center[0])
     x = np.arange(0, trial_range)
-    ax[0].set_title('Front & Back Y (blue), Side Y (yellow), Over Time')
-    ax[1].set_title('Front & Back X (blue), Side X (yellow), Over Time')
-    ax[2].set_title('Front & Back Z (blue), Side Z (yellow), Over Time')
+
     xav = 0
     yav = 0
     zav = 0
     points = 0
     average_points = []
+    average_femur_human = []
+    average_femur_side = []
+
+    average_tibia_human = []
+    average_tibia_side = []
 
     for i in range(trial_range):
-        my_maker = Point.Point(0, 0, 0)
-        side_maker = Point.Point(0, 0, 0)
-        for marker in marking:
-            my_maker += marker[i]
+        human_femur = Point.Point(0, 0, 0)
+        exo_femur = Point.Point(0, 0, 0)
+        human_tibia = Point.Point(0, 0, 0)
+        exo_tibia = Point.Point(0, 0, 0)
 
-        for mark in markingSide:
-            side_maker += mark[i]
-        my_maker = my_maker / len(marking)
-        side_maker = side_maker / len(markingSide)
+        for marker in femur_center:
+            human_femur += marker[i]
 
-        average_points.append(my_maker)
-        #Y
-        ax[0].plot(i, my_maker.y, 'bo', markersize=1)
-        #ax[0].plot(i, my_maker.x, 'bo', markersize=1)
-        ax[0].plot(i, side_maker.y, 'yo', markersize=1)
+        for mark in femur_side:
+            exo_femur += mark[i]
 
-        # X
-        #ax[1].plot(i, my_maker.z, 'ro', markersize=1)
-        ax[1].plot(i, my_maker.x, 'bo', markersize=1)
-        ax[1].plot(i, side_maker.x, 'yo', markersize=1)
+        for marker in tibia_center:
+            human_tibia += marker[i]
 
-        # Z
-        #ax[2].plot(i, my_maker.y, 'ro', markersize=1)
-        ax[2].plot(i, my_maker.z, 'bo', markersize=1)
-        ax[2].plot(i, side_maker.z, 'yo', markersize=1)
+        for mark in tibia_side:
+            exo_tibia += mark[i]
 
-        xav = xav + my_maker.x
-        yav = yav + my_maker.y
-        zav = zav + my_maker.z
-        points = points + 1
+        human_femur = human_femur / len(femur_center)
+        exo_femur = exo_femur / len(femur_side)
 
-    xav = xav / points
-    yav = yav / points
-    zav = zav / points
+        average_femur_human.append(human_femur)
+        average_femur_side.append(exo_femur)
+
+
+        human_tibia = human_tibia / len(tibia_center)
+        exo_tibia = exo_tibia / len(tibia_side)
+
+        average_tibia_human.append(human_tibia)
+        average_tibia_side.append(exo_tibia)
+
+
+
+    average_femur_side = PointArray.PointArray.from_point_array(average_femur_side)
+    average_femur_human = PointArray.PointArray.from_point_array(average_femur_human)
+
+    average_tibia_side = PointArray.PointArray.from_point_array(average_tibia_side)
+    average_tibia_human = PointArray.PointArray.from_point_array(average_tibia_human)
+
+    t = np.linspace(0, 100, len(average_femur_human))
+
+    fig, ax = plt.subplots(3,2)
+    ax[0,0].plot(t, average_femur_human.x, 'b', markersize=1)
+    ax[0,0].plot(t, average_femur_side.x, 'r', markersize=1)
+    ax[1,0].plot(t, average_femur_human.y, 'b', markersize=1)
+    ax[1,0].plot(t, average_femur_side.y, 'r', markersize=1)
+    ax[2,0].plot(t, average_femur_human.z, 'b', markersize=1)
+    ax[2,0].plot(t, average_femur_side.z, 'r', markersize=1)
+
+    ax[0,1].plot(t, average_tibia_human.x, 'b', markersize=1)
+    ax[0,1].plot(t, average_tibia_side.x, 'r', markersize=1)
+    ax[1,1].plot(t, average_tibia_human.y, 'b', markersize=1)
+    ax[1,1].plot(t, average_tibia_side.y, 'r', markersize=1)
+    ax[2,1].plot(t, average_tibia_human.z, 'b', markersize=1)
+    ax[2,1].plot(t, average_tibia_side.z, 'r', markersize=1)
+
+    fig.suptitle('Comparison of the human thigh position and the exoskeleton thigh position', fontsize=16)
+    ax[0,0].set_title("X Thigh Position", fontsize=16)
+    ax[1,0].set_title("Y Thigh Position", fontsize=16)
+    ax[2,0].set_title("Z Thigh Position", fontsize=16)
+
+    ax[0,1].set_title("X Shank Position", fontsize=16)
+    ax[1,1].set_title("Y Shank Position", fontsize=16)
+    ax[2,1].set_title("Z Shank Position", fontsize=16)
+
+
+    ax[0,0].legend(["Human", "Exoskeleton"])
+    ax[0,0].set_ylabel("Position (mm)", fontsize=16)
+    ax[1,0].set_ylabel("Position (mm)", fontsize=16)
+    ax[2,0].set_ylabel("Position (mm)", fontsize=16)
+    ax[2,0].set_xlabel("Trial %", fontsize=16)
+    ax[2,1].set_xlabel("Trial %", fontsize=16)
+
+
     plt.show()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
